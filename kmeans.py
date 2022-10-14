0cc22b67c64fecb122fa90c9b41eb0877dd0caff
skrbics <srdjan.skrbic@dmi.uns.ac.rs>
2022-10-14 14:07:33 +0200

    Initial commit

---
+++ kmeans.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(data)
+
+plt.scatter(x, y, c=kmeans.labels_)
+plt.show()
---
+++ main.py
@@ -0,0 +1,1 @@
+print("Podatkovno rudarjenje :)")
